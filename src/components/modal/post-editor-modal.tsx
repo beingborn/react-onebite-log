a62064dbbea91faf6ce0b9de705d602dfbca0711
beingborn <dolmin0708@naver.com>
2025-11-17 22:21:26 +0900

    Create Post Feature

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -1,11 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
+import { generateErrorMessage } from "@/lib/error";
 import { usePostEditorModal } from "@/store/post-editor-modal";
 import { ImageIcon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
 
 export default function PostEditorModal() {
     const [content, setContent] = useState("");
+
+    const { mutate: createPost, isPending: isCreatePostIsPending } =
+        useCreatePost({
+            onSuccess: () => {
+                toast.info("성공", {
+                    position: "top-center",
+                });
+
+                close();
+            },
+            onError(error) {
+                const message = generateErrorMessage(error);
+
+                toast.error(message, {
+                    position: "top-center",
+                });
+                setContent("");
+            },
+        });
 
     const { isOpen, close } = usePostEditorModal();
 
@@ -13,6 +35,12 @@ export default function PostEditorModal() {
 
     const handleCloseModal = () => {
         close();
+    };
+
+    const handleCreatePostClick = () => {
+        if (content.trim() === "") return;
+
+        createPost(content);
     };
 
     useEffect(() => {
@@ -34,17 +62,23 @@ export default function PostEditorModal() {
             <DialogContent className="max-h-[90vh]">
                 <DialogTitle>포스트 작성</DialogTitle>
                 <textarea
+                    disabled={isCreatePostIsPending}
                     ref={textareaRef}
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
                     className="max-h-125 min-h-25 focus:outline-none"
                     placeholder="무슨 일이 있었나요?"
                 />
-                <Button variant="outline">
+                <Button variant="outline" disabled={isCreatePostIsPending}>
                     <ImageIcon />
                     이미지 추가
                 </Button>
-                <Button>저장</Button>
+                <Button
+                    disabled={isCreatePostIsPending}
+                    onClick={handleCreatePostClick}
+                >
+                    저장
+                </Button>
             </DialogContent>
         </Dialog>
     );
