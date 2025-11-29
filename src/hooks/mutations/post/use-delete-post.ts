60e3b25487fcdaec864acc7f2cac5f85d49c3f0e
beingborn <dolmin0708@naver.com>
2025-11-30 01:53:01 +0900

    Cache Control Data, Post RLS Setting

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImagesInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { useMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: useMutationCallback) {
+    const queryClient = useQueryClient();
+
     return useMutation({
         mutationFn: deletePost,
         onSuccess: async (deletedPost) => {
@@ -14,6 +17,11 @@ export function useDeletePost(callbacks?: useMutationCallback) {
                     `${deletedPost.author_id}/${deletedPost.id}`,
                 );
             }
+
+            // 1. 캐시 초기화
+            queryClient.resetQueries({
+                queryKey: QUERY_KEYS.post.list,
+            });
         },
         onError: (error) => {
             if (callbacks?.onError) callbacks.onError(error);
